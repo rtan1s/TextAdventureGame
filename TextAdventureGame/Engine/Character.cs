ef981fb8f43e16362485c963a10099d13c67fad5
rtan1s <r0b3rt0xx@gmail.com>
2017-03-21 16:06:25 +0100

    RT: Version 0.001 - Added Some Classes and Constructors

---
+++ TextAdventureGame/Engine/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAGEngine
+{
+    public class Character
+    {
+        public int intCharMoney { get; set; }
+        public int intCharLevel { get; set; }
+        public int intCharExpPoints { get; set; }
+        public int intCharEnergy { get; set; }
+        public int intCharKnowledge { get; set; }
+        public int intCharCunning { get; set; }
+        public int intCharSelfDefense { get; set; }
+        public int intCharAttack { get; set; }
+    }
+}
