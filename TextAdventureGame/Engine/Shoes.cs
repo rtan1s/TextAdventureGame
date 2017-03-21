1ab0d4827957c7e0a6ba6ec3d4e02d22653c34f5
rtan1s <r0b3rt0xx@gmail.com>
2017-03-21 14:01:53 +0100

    T 0.00001

---
+++ TextAdventureGame/Engine/Shoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAGEngine
+{
+    class Shoes
+    {
+        public string strShoeName { get; set; }
+        public string strShoeGender { get; set; }
+        public string strShoeType { get; set; }
+        public string strShoeDescription { get; set; }
+    }
+}
